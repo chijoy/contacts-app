f5320858be6971d7554eefbb4235cfc62132795c
Joy <joymccaffreychicago@gmail.com>
2017-05-10 22:01:51 -0500

    Add model, controller, and views.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,43 @@
 class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.create(
+                              name: params[:name],
+                              email: params[:email],
+                              phone: params[:phone])
+
+      redirect_to '/'
+  end
+
+  def show
+    @contact = Contact.find_by(id: params[:id])
+  end
+
+  def edit
+    @contact = Contact.find_by(id: params[:id])
+  end
+
+  def update
+    @contact = Contact.find_by(id: params[:id])
+    @contact.update(
+                    name: params[:name],
+                    email: params[:email],
+                    phone: params[:phone])
+
+    redirect_to "/contacts/#{@contact.id}"
+  end
+
+  def destroy
+    @contact = Contact.find_by(id: params[:id])
+    @contact.destroy
+
+    redirect_to '/'
+  end
 end
