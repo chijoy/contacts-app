3fd354d3ea8c45645f1bcd072db6523c29a323b4
Joy <joymccaffreychicago@gmail.com>
2017-05-10 20:59:07 -0500

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Contact.create([{name: "Jim", email: "jim@gmail.com", phone: "773-555-1212"}])
+
+Contact.create([{name: "Joe", email: "joe@gmail.com", phone: "773-555-2345"}])
+
+Contact.create([{name: "Bob", email: "bob@gmail.com", phone: "773-555-4567"}])
+
+Contact.create([{name: "Jane", email: "jane@gmail.com", phone: "773-555-3579"}])
